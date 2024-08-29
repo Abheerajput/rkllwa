4ca1fd0bc90663f7a4ad2941df208cba2ee7d65e
deepsgandhi <deepanshu@dofinity.com>
2024-08-29 13:10:39 +0530

    header-icon

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -64,9 +64,9 @@ const Header = ({ backgroundImg, logo, menuIcon, mainTitle, mainDescription, but
           backgroundImage: `url(${backgroundImg})`,
         }}
       >
-        <div className='absolute pl-[3.4%] xs:mt-4 xs:top-[11%] sx:top-[9%] sm:top-[12%] md:top-[12%] top-[9%]'>
+        <div className='absolute pl-[3.4%] xs:mt-4 xs:top-[11%] sx:top-[9%] sm:top-[12%] md:top-[12%] top-[12%]'>
           <Link to="/">
-            <img src={logo} alt="Logo" className="w-[365px] xs:w-[250px] xs:-[250px]  h-[175px]" />
+            <img src={logo} alt="Logo" className="w-[365px]  xs:w-[250px] xs:-[250px]  h-[175px]" />
           </Link>
         </div>
         <div className="flex px-[5.2%] overflow-x-hidden justify-end   p-8 ">
